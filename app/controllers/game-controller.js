b38e48ecae85c1a7d31fed1ac832dbaed461dea1
An-AngryBear <monahajt@gmail.com>
2017-08-07 23:04:09 -0500

    added custom BBCode replacers

--- app/controllers/game-controller.js
+++ app/controllers/game-controller.js
@@ -14,30 +14,45 @@ patchNotesApp.controller('PatchNotesController', function($scope, $rootScope, $r
 
 	$scope.filterFn = function(news) {
 	    // Do some tests
-	    console.log("news?", news);
-	    if(news.title.search(/update|fix|expansion|build|patch/i) >= 0) {
-	    	console.log("if called?");
+	    if(news.title.search(/update|fix|expansion|build|patch|\.0|\.1|\.2|\.3|\.4|\.5|\.6|\.7|\.8|\.9/i) >= 0) {
 	        return true; // this will be listed in the results
 	    }
 	    return false; // otherwise it won't be within the results
 	};
 
+	let findAndReplaceExtraBBCode = (news) => {
+		let replaced = news.replace(/&#91;/g, "[")
+			.replace(/&#93;/g, "]")
+			.replace(/\[h1]/g, "<h1>")
+			.replace(/\[\/h1]/g, "</h1>")
+			.replace(/\[\/\*]/g, "")
+			.replace(/\[\/olist\]/g, "</ol>")
+			.replace(/\[olist..]/g, "<ol>");
+		// newsObj.contents = newsObj.contents.replace(/\[\/h1]/g, "</h1>");
+		// newsObj.contents = newsObj.contents.replace(/\[\/\*]/g, "");
+		console.log(replaced, "please work");
+		return replaced;
+	};
+
 	// TODO: expand bbcode search terms; also change [h1] to <h1> and remove [/*]
 	let parseBBCode = (newsArray) => {
 		let parsedCode = newsArray.map( (newsObj) => {
-			if (newsObj.contents.search(/\[img]|\[b]|\[\*]|\[list]/g) >= 0) {
-				console.log("before parse", newsObj);
+			console.log("pre filtered", newsObj);
+			if (newsObj.contents.search(/\[img]|\[b]|\[\*]|\[url|\[list]/g) >= 0) {
+				// console.log("before parse", newsObj);
 				var result = XBBCODE.process({
 				      text: newsObj.contents,
 				      removeMisalignedTags: true,
 				      addInLineBreaks: true
 				    });
-				newsObj.contents = result.html;
+				let filteredResult = findAndReplaceExtraBBCode(result.html);
+				newsObj.contents = filteredResult;
 			}
 			return newsObj;
 		});
 		return parsedCode;
 	};
+
 
 	let displayPatchNotes = () => {
 		console.log($routeParams.appid);
